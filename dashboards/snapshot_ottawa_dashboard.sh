911e34d58940713a993fefcbd73908e721cdca82
David Pritchard <davidp@mentalcanvas.com>
2021-03-24 22:36:27 -0400

    Updated dashboard scraper, belatedly...

--- dashboards/snapshot_ottawa_dashboard.sh
+++ dashboards/snapshot_ottawa_dashboard.sh
@@ -2,19 +2,19 @@
 #!/bin/sh
 CHROME="/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
 DATE=`date +"%Y-%m-%d"`
-BASEURL="https://app.powerbi.com/view?r=eyJrIjoiMTBhZTU4YmUtNGI1MC00ZGZjLTgzOGYtNGFiZjdiNmIzNDk3IiwidCI6ImRmY2MwMzNkLWRmODctNGM2ZS1hMWI4LThlYWE3M2YxYjcyZSJ9"
+BASEURL="https://app.powerbi.com/view?r=eyJrIjoiNmQ3YzRiZGEtYTAyNC00YjgyLWEwNGMtMjQ4YWJjY2JmZGVlIiwidCI6ImRmY2MwMzNkLWRmODctNGM2ZS1hMWI4LThlYWE3M2YxYjcyZSJ9"
 "$CHROME" --headless --disable-gpu --virtual-time-budget=5000 \
     --print-to-pdf=dashboard_${DATE}_page1.pdf \
-    "$BASEURL&pageName=ReportSectiond3db0329f5a43a351b64"
+    "$BASEURL"
 "$CHROME" --headless --disable-gpu --virtual-time-budget=10000 \
     --print-to-pdf=dashboard_${DATE}_page2.pdf \
-    "$BASEURL&pageName=ReportSection589411c28cc80d80cc14"
+    "$BASEURL&pageName=ReportSection4d8d09f90ed9cc5513d5"
 "$CHROME" --headless --disable-gpu --virtual-time-budget=5000 \
     --print-to-pdf=dashboard_${DATE}_page3.pdf \
-    "$BASEURL&pageName=ReportSection3f159c9ca4691f023509"
+    "$BASEURL&pageName=ReportSectionf2da438ea5d04a89daeb"
 "$CHROME" --headless --disable-gpu --virtual-time-budget=10000 \
     --print-to-pdf=dashboard_${DATE}_page4.pdf \
-    "$BASEURL&pageName=ReportSectionc4838ab7bc44487dadbf"
+    "$BASEURL&pageName=ReportSection70be3fe934dac4a89802"
 "$CHROME" --headless --disable-gpu --virtual-time-budget=10000 \
     --print-to-pdf=dashboard_${DATE}_page5.pdf \
-    "$BASEURL&pageName=ReportSectionf29e3e3cd2143ed056b2"
+    "$BASEURL&pageName=ReportSection0e9cac292303a6058854"
